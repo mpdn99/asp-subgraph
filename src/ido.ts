36e1aa024a3a03276271c9898439ff64013e2e0e
mpdn99 <mpdn99@gmail.com>
2022-07-27 13:02:03 +0700

    init

--- src/ido.ts
+++ src/ido.ts
@@ -1,55 +1,30 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  IDO,
-  AdminChanged,
-  BeaconUpgraded,
-  Upgraded
+  LogBuy
 } from "../generated/IDO/IDO"
-import { ExampleEntity } from "../generated/schema"
+import { Buy, Report } from "../generated/schema"
 
-export function handleAdminChanged(event: AdminChanged): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleLogBuy(event: LogBuy): void {
+  let buy = new Buy(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
+  buy.projectSlug = event.params.projectSlug
+  buy.refSlug = event.params.refSlug
+  buy.buyAmount = event.params.buyAmount
+  buy.paymentAmount = event.params.paymentAmount
+  buy.systemProfit = event.params.systemProfit
+  buy.save()
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let report = Report.load(event.params.projectSlug.concat(event.params.refSlug))
+  if (!report) {
+    report = new Report(event.params.projectSlug.concat(event.params.refSlug))
+    report.totalBuyAmount = new BigInt(0)
+    report.totalPaymentAmount = new BigInt(0)
+    report.totalSystemProfit = new BigInt(0)
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.admin(...)
-  // - contract.implementation(...)
+  report.projectSlug = event.params.projectSlug
+  report.refSlug = event.params.refSlug
+  report.totalBuyAmount =  report.totalBuyAmount.plus(event.params.buyAmount)
+  report.totalPaymentAmount = report.totalPaymentAmount.plus(event.params.paymentAmount)
+  report.totalSystemProfit = report.totalSystemProfit.plus(event.params.systemProfit)
+  report.save()
 }
 
-export function handleBeaconUpgraded(event: BeaconUpgraded): void {}
-
-export function handleUpgraded(event: Upgraded): void {}
